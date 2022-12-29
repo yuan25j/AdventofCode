9ae2a0ab78c9abc9ba246a9d94b6b4b739972435
John Yuan <89277444+yuan25j@users.noreply.github.com>
2022-12-29 17:32:02 -0500

    day 3 completed

--- adventof code/day3.py
+++ adventof code/day3.py
@@ -1,4 +1,4 @@
-my_file = open("input3.txt", "r")
+my_file = open("adventof code\input3.txt", "r")
   
 # reading the file
 data = my_file.read()
@@ -6,10 +6,56 @@ data = my_file.read()
 # replacing end splitting the text 
 # when newline ('\n') is seen.
 data_into_list = data.split("\n")
-print(data_into_list)
 my_file.close()
-for x in data_into_list:
-    first = x[0:len(x)/2]
-    last = x[len(x)/2-1:len(x)-1]
-    print(first)
+result = 0
+hash = {}
+i = ord("A")
+j = 27
+while i <= ord("Z"):
+    hash[chr(i)] = j
+    i += 1
+    j += 1
+i = ord("a")
+j = 1
+while i <= ord("z"):
+    hash[chr(i)] = j
+    i += 1
+    j += 1
+print(hash)
+#code for part 2
+templst = []
+for i in range (len(data_into_list)):
+    print(i)
+    templst.append(data_into_list[i])
+    print(templst)
+    if (i+1) % 3 == 0:
+        for x in templst[0]:
+            if x in templst[1] and x in templst[2]:
+                # print(x)
+                # print(hash[x])
+                result += hash[x]
+                # print(result)
+                templst = []
+                break
+print(result)
+        
     
+        
+        
+
+#below code is for part 1
+# for x in data_into_list:
+#     first = x[0:int(len(x)/2)]
+#     last = x[int(len(x)/2):len(x)]
+#     #print(first)
+#     #print(last)
+#     for z in first:
+#         if z in last:
+#             #print(z)
+#             #print(hash[z])
+#             result += hash[z]
+#             #print(result)
+#             break
+        
+   
+# print(result)
